08c6df336ae0e49b653c9ff8c61fb3d530962043
brett <brettbedarf.dev@gmail.com>
2020-11-01 23:25:08 -0700

    finish challenge 3 - add and delete notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,43 @@
-import React from "react";
-import Header from "./Header";
-import Footer from "./Footer";
-import Note from "./Note";
-import CreateArea from "./CreateArea";
+import React, { useState } from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import Note from './Note';
+import CreateArea from './CreateArea';
 
 function App() {
-  return (
-    <div>
-      <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
-      <Footer />
-    </div>
-  );
+	const [notes, setNotes] = useState([]);
+
+	function addNote(note) {
+		console.log('adding note');
+		setNotes(prev => {
+			return [...prev, note];
+		});
+		console.log(notes);
+	}
+
+	function deleteNote(id) {
+		setNotes([...notes.slice(0, id), ...notes.slice(id + 1, notes.length)]);
+	}
+
+	return (
+		<div>
+			<Header />
+			<CreateArea addNote={addNote} />
+			<div className='notes-area'>
+				{notes.map((note, index) => (
+					<Note
+						key={index}
+						id={index}
+						title={note.title}
+						content={note.content}
+						deleteNote={deleteNote}
+					/>
+				))}
+			</div>
+
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
