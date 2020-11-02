03a46e2d4f725b6cc824a5e820b763881bd1643e
brett <brettbedarf.dev@gmail.com>
2020-11-01 23:25:08 -0700

    add and delete notes

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,15 +1,54 @@
-import React from "react";
+import React, { useState } from 'react';
 
-function CreateArea() {
-  return (
-    <div>
-      <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
-      </form>
-    </div>
-  );
+function CreateArea(props) {
+	const [inputText, setInput] = useState({});
+
+	function handleChange(event) {
+		const { name, value } = event.target;
+
+		setInput({
+			...inputText,
+			[name]: value,
+		});
+	}
+
+	function submitNote(e) {
+		props.addNote(inputText);
+		//set all current properties to empty strings
+		const inputToClear = { ...inputText };
+		Object.keys(inputToClear).forEach(key => (inputToClear[key] = ''));
+		setInput(inputToClear);
+		e.preventDefault();
+	}
+
+	return (
+		<div>
+			<form onSubmit={submitNote}>
+				<input
+					name='title'
+					placeholder='Title'
+					onChange={handleChange}
+					value={
+						typeof inputText.title !== 'undefined'
+							? inputText.title
+							: ''
+					}
+				/>
+				<textarea
+					name='content'
+					placeholder='Take a note...'
+					rows='3'
+					onChange={handleChange}
+					value={
+						inputText.content !== 'undefined'
+							? inputText.content
+							: ''
+					}
+				/>
+				<button type='submit'>Add</button>
+			</form>
+		</div>
+	);
 }
 
 export default CreateArea;
